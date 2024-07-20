8cdfc0840635f94360c6b88276afbcfb47dc6cd8
hk28 <heinrich.klepper@gmail.com>
2024-07-20 22:33:33 +0200

    fix themes

--- dot_config/nvim/lua/custom/plugins/themery.lua
+++ dot_config/nvim/lua/custom/plugins/themery.lua
@@ -1,17 +1,18 @@
 return {
-  'zaldih/themery.nvim',
-  -- Minimal config
-  opts = {
-    themes = {
-      --'gruvbox',
-      'rose-pine',
-      'tokyonight',
-      'catppuccin',
-    }, -- Your list of installed colorschemes.
-    livePreview = true, -- Apply theme while picking. Default to true.
-  },
+	"zaldih/themery.nvim",
+	priority = 1000,
+	-- Minimal config
+	opts = {
+		themes = {
+			--'gruvbox',
+			"rose-pine",
+			"tokyonight",
+			"catppuccin",
+		}, -- Your list of installed colorschemes.
+		livePreview = true, -- Apply theme while picking. Default to true.
+	},
 
-  keys = {
-    { '<leader>uy', '<cmd>Themery<cr>', desc = 'Change color theme' },
-  },
+	keys = {
+		{ "<leader>uy", "<cmd>Themery<cr>", desc = "Change color theme" },
+	},
 }
--- dot_config/nvim/lua/custom/plugins/themes.lua
+++ dot_config/nvim/lua/custom/plugins/themes.lua
@@ -1,4 +1,24 @@
 return {
-  { 'rose-pine/neovim', name = 'rose-pine' },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("rose-pine")
+
+			-- You can configure highlights by doing something like:
+			-- vim.cmd.hi 'Comment gui=none'
+		end,
+		-- opts = {
+		-- 	styles = {
+		-- 		bold = true,
+		-- 		italic = false,
+		-- 		transparency = false,
+		-- 	},
+		-- },
+	},
+	{ "catppuccin/nvim", name = "catppuccin" }, --, priority = 1000 },
 }
