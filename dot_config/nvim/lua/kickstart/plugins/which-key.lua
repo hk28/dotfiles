b39fe3ecab5627ac9eca177500200ee9179dd02a
Klepper Heinrich GA <heinrich.klepper@mt.com>
2024-08-01 14:59:40 +0200

    MTGA

--- dot_config/nvim/lua/kickstart/plugins/which-key.lua
+++ dot_config/nvim/lua/kickstart/plugins/which-key.lua
@@ -14,24 +14,53 @@
 --  config = function() ... end
 
 return {
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    -- version = '2.1.0',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-      local wk = require 'which-key'
-      wk.add { --register { -- add for 3.x
-        { '<leader>u', group = 'ui' },
-        { '<leader>b', group = 'buffer' },
-        { '<leader>c', group = 'code' },
-        { '<leader>g', group = 'git' },
-        { '<leader>h', group = 'git hunk' },
-        { '<leader>d', group = 'diag/quickfix' },
-        { '<leader>t', group = 'terminal' },
-        { '<leader>ut', '<cmd>TransparentToggle<cr>', desc = 'Toggle transparent bg', mode = 'n' },
-      }
-    end,
-  },
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		-- version = '2.1.0',
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			require("which-key").setup()
+			local wk = require("which-key")
+			wk.add({ --register { -- add for 3.x
+				{ "<leader>u", group = "ui" },
+				{ "<leader>b", group = "buffer" },
+				{ "<leader>c", group = "code" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>h", group = "git hunk" },
+				{ "<leader>d", group = "diag/quickfix" },
+				{ "<leader>t", group = "terminal" },
+				{ "<leader>ut", "<cmd>TransparentToggle<cr>", desc = "Toggle transparent bg", mode = "n" },
+			})
+		end,
+	},
 }
+
+-- https://github.com/folke/which-key.nvim/issues/71
+-- vim.cmd(([[
+-- autocmd FileType rst lua whichkeyrST()
+-- autocmd FileType markdown lua whichkeyMarkdown()
+-- ]]))
+--
+-- _G.whichkeyMarkdown = function()
+--   local wk = require("which-key")
+--   local buf = vim.api.nvim_get_current_buf()
+--   wk.register({
+--     ["<C-s>"] = {
+--       name = "Markdown",
+--       ["0"] = { "<cmd>echo 'markdown header'<cr>", "Create header", buffer = buf },
+--     },
+--   })
+-- end
+--
+-- _G.whichkeyrST = function()
+--   local wk = require("which-key")
+--   local buf = vim.api.nvim_get_current_buf()
+--   wk.register({
+--     ["<C-s>"] = {
+--       name = "reStructuredText",
+--       ["0"] = { "<cmd>echo 'rST header'<cr>", "Create header", buffer = buf },
+--     },
+--   })
+-- end
+--
 -- vim: ts=2 sts=2 sw=2 et
