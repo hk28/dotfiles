5b9e7691fc2b0f4fb8b71b9dba6674cbf4712da1
spock <spock@tinker.tinker.net>
2024-08-13 17:43:19 +0200

    tinker, neotest

--- dot_config/nvim/lua/custom/plugins/git.lua
+++ dot_config/nvim/lua/custom/plugins/git.lua
@@ -13,4 +13,19 @@ return {
 		config = true,
 		opts = {},
 	},
+	-- {
+	-- 	"SuperBo/fugit2.nvim",
+	-- 	opts = {
+	-- 		width = 100,
+	-- 	},
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- 	cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph" },
+	-- 	keys = {
+	-- 		{ "<leader>F", mode = "n", "<cmd>Fugit2<cr>" },
+	-- 	},
+	-- },
 }
